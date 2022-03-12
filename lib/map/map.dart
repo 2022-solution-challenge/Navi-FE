6bf0ddf2f2d717138a0458a48dc32b2a7c507b36
pell13 <peixueying@gmail.com>
2022-03-12 22:44:57 +0900

    Add: Custom infowindow in map

--- lib/map/map.dart
+++ lib/map/map.dart
@@ -1,11 +1,14 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:clippy_flutter/triangle.dart';
+import 'package:custom_info_window/custom_info_window.dart';
+
 import 'dart:async';
 import 'dart:math';
 // import 'package:geolocator/geolocator.dart';
 
-class MapApp extends StatelessWidget{
+class MapApp extends StatelessWidget {
   const MapApp({Key? key}) : super(key: key);
 
   @override
@@ -21,8 +24,6 @@ class MapApp extends StatelessWidget{
     // TODO: implement build
     throw UnimplementedError();
   }
-
-
 }
 
 class MapSample extends StatefulWidget {
@@ -36,7 +37,6 @@ class MapSample extends StatefulWidget {
 
 // }
 class MapSampleState extends State<MapSample> {
-
   // Future<Position> getLocation() async{
   //   Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
   //   return position;
@@ -44,22 +44,85 @@ class MapSampleState extends State<MapSample> {
 
   // Future<Position> position = getLocation();
 
+  CustomInfoWindowController _customInfoWindowController = CustomInfoWindowController();
+
   late GoogleMapController _controller; //컨트롤러
+
+  @override
+  void dispose() {
+    _customInfoWindowController.dispose();
+    super.dispose();
+  }
+
 
   // 지도 클릭 시 표시할 장소에 대한 마커 목록
   final List<Marker> markers = [];
 
-  addMarker(cordinate) {
+  showMarker(cordinate) {
     int id = Random().nextInt(100);
     setState(() {
+      _customInfoWindowController.hideInfoWindow!();
+      markers.clear(); //전에 찍은 곳은 안보이게
       markers
-          .add(Marker(position: cordinate, markerId: MarkerId(id.toString())));
+          .add(
+            Marker(position: cordinate,
+            markerId: MarkerId(id.toString()),
+            onTap: () {
+            _customInfoWindowController.addInfoWindow!(
+            Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.account_circle,
+                            color: Colors.white,
+                            size: 30,
+                          ),
+                          SizedBox(
+                            width: 8.0,
+                          ),
+                          Text(
+                            "I am here",
+                            style:
+                                Theme.of(context).textTheme.headline6?.copyWith(
+                                      color: Colors.white,
+                                    ),
+                          )
+                        ],
+                      ),
+                    ),
+                    width: double.infinity,
+                    height: double.infinity,
+                  ),
+                ),
+                Triangle.isosceles(
+                  edge: Edge.BOTTOM,
+                  child: Container(
+                    color: Colors.blue,
+                    width: 20.0,
+                    height: 10.0,
+                  ),
+                ),
+              ],
+            ),
+            cordinate,
+          );
+        },
+            ));
     });
   }
 
-
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     // ignore: sized_box_for_whitespace
     final med = MediaQuery.of(context).size;
     return Scaffold(
@@ -68,34 +131,44 @@ class MapSampleState extends State<MapSample> {
         elevation: 0.0,
         toolbarHeight: 50,
       ),
-      body: Container(
-        width: med.width,
+      body: Stack(
         // height: MediaQuery.of(context).size.height,
         // width : 300,
-        height: med.height,
-        child: GoogleMap(
+        children: <Widget>[
+        GoogleMap(
+          onTap: (position) {
+              _customInfoWindowController.hideInfoWindow!();
+            },
+          onCameraMove: (position) {
+              _customInfoWindowController.onCameraMove!();
+            },
           mapType: MapType.normal,
           onMapCreated: (controller) {
             setState(() {
               _controller = controller;
+              _customInfoWindowController.googleMapController = controller;
             });
           },
           markers: markers.toSet(),
           initialCameraPosition: CameraPosition(
-              target: LatLng(
-                  37.5,
-                  126.9294254 // 시작 위치
-              ),
-              zoom: 18,
-            ),
-          onTap: (cordinate) {
+            target: LatLng(37.5, 126.9294254 // 시작 위치
+                ),
+            zoom: 18,
+          ),
+          onLongPress: (cordinate) {
             _controller.animateCamera(CameraUpdate.newLatLng(cordinate));
-            addMarker(cordinate);
+            showMarker(cordinate);
           },
+          
         ),
+        CustomInfoWindow(
+            controller: _customInfoWindowController,
+            height: 75,
+            width: 150,
+            offset: 50,
+          ),
+        ]
       ),
     );
-
   }
-
 }
