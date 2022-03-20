9e0774816330d1c32fc52a5ac9f06f23c3db8e2a
hyojaeKwon <khj010909@gm.gist.ac.kr>
2022-03-21 02:25:49 +0900

    google map search api class

--- lib/navi/mapDest.dart
+++ lib/navi/mapDest.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import './place.dart';
+import 'package:http/http.dart' as http;
 
 class MapDest extends StatelessWidget{
   const MapDest({Key? key, @required this.dest}) : super(key: key);
@@ -22,6 +26,23 @@ class SearchData extends StatelessWidget{
   final String? data;
   //init에서 http결과 가져오기
   //https://developers.google.com/maps/documentation/places/web-service/search-text?hl=ko
+  Future<Place> fetchPost() async {
+    var url = Uri.parse(
+        'https://maps.googleapis.com/maps/api/place/textsearch/json'
+        + '?query=$data%20in%20Sydney'
+        + '&key=YOUR_API_KEY'    );
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      // 만약 서버가 OK 응답을 반환하면, JSON을 파싱합니다.
+      return Place.fromJson(json.decode(response.body));
+    } else {
+      // 만약 응답이 OK가 아니면, 에러를 던집니다.
+      throw Exception('Failed to load post');
+    }
+  }
+  //얘를 또 parse 해야함
+
 
   @override
   Widget build(BuildContext context){
