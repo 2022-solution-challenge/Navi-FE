ea459201cee249e3e4c5a6e44a31277f821e87d3
hyojaeKwon <khj010909@gm.gist.ac.kr>
2022-03-21 02:16:27 +0900

    google map search api class

--- lib/navi/mapDest.dart
+++ lib/navi/mapDest.dart
@@ -10,7 +10,7 @@ class MapDest extends StatelessWidget{
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SearchData(),
+      home: SearchData(data: dest,),
     );
   }
 }
@@ -18,14 +18,22 @@ class MapDest extends StatelessWidget{
 
 //검색 결과 보여주는
 class SearchData extends StatelessWidget{
-  const SearchData({Key? key}) : super(key: key);
-
+  const SearchData({Key? key, @required this.data}) : super(key: key);
+  final String? data;
   //init에서 http결과 가져오기
+  //https://developers.google.com/maps/documentation/places/web-service/search-text?hl=ko
 
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: Text("dest"),
+      body: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 30.0,
+          ),
+          Text("$data")
+        ],
+      )
     );
   }
 }
