2b30a0a73c71b893387f69ce3203c088eb2f846e
hyojaeKwon <khj010909@gm.gist.ac.kr>
2022-03-20 16:36:49 +0900

    remake app function

--- lib/map/map.dart
+++ lib/map/map.dart
@@ -1,95 +1,46 @@
-// ignore_for_file: prefer_const_constructors
-import 'dart:developer';
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'dart:async';
-import 'package:geolocator/geolocator.dart';
-import 'package:location/location.dart';
-import 'package:http/http/dart' as http;
 
 class MapApp extends StatelessWidget{
   const MapApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Center(
-      child: MaterialApp(
-        title: 'Google Maps',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: MapSample(),
+      child: Column(
+        children: <Widget>[
+          Text("Where to Go? SEARCH"),
+          SizedBox(
+            height: 20.0,
+          ),
+          ToGoInput()
+        ],
       ),
     );
-    // TODO: implement build
-    throw UnimplementedError();
   }
-
-
 }
 
-class MapSample extends StatefulWidget {
-  const MapSample({Key? key}) : super(key: key);
+
+//Make StateFul widget
+class ToGoInput extends StatefulWidget{
+  const ToGoInput({Key? key}) : super(key: key);
 
   @override
-  State<MapSample> createState() => MapSampleState();
+  State<ToGoInput> createState() => ToGoInputState();
 }
 
+class ToGoInputState extends State<ToGoInput>{
 
-
-
-
-
-class MapSampleState extends State<MapSample> {
-  List<Marker> _markers = [];
-
+  final textController = TextEditingController();
+  String textToFind = "";
   @override
   void initState(){
-    _markers.add(Marker(
-      markerId: MarkerId("first marker"),
-      draggable: true,
-      onTap: () => print("Tappend"),
-      position: LatLng(
-          37.5283169,
-          126.9294254
-      )
-    ));
+
   }
-
-
   @override
   Widget build(BuildContext context){
-    // ignore: sized_box_for_whitespace
-    // med => media query calculate
-    final med = MediaQuery.of(context).size;
-    final textController = TextEditingController();
-    String? textToFind;
-    // _locationData = locateUser();
-
     return Scaffold(
-
-      body: Column(
-        children: <Widget>[
-          Container(
-            width: med.width,
-            // height: MediaQuery.of(context).size.height,
-            // width : 300,
-            height: med.height*0.6,
-            child: GoogleMap(
-              mapType: MapType.normal,
-              markers: Set.from(_markers),
-              initialCameraPosition: CameraPosition(
-                target: LatLng(
-                  // _locationData.latitude,
-                  // _locationData.longitude
-                    37.5283169,
-                    126.9294254
-                ),
-                zoom: 18,
-              ),
-            ),
-          ),
+      body: ListView(
+        children : [
           Row(
             children: <Widget>[
               Expanded(
@@ -101,35 +52,21 @@ class MapSampleState extends State<MapSample> {
                   ),
                 ),
               ),
+
               SizedBox(
                 width: 20.0,
               ),
+
               OutlinedButton(
                 onPressed: (){
                   textToFind = textController.text;
-                  //Respond to button press
                 },
                 child: Text("Find"),
-              )
+              ),
             ],
-          )
-
-
-        ],
-      )
+          ),
+        ]
+      ),
     );
-
   }
-
-}
-
-//location data가져오는 부분
-void getLocationData() async {
-  String url = "";
-  var response = await http.get(url);
-  var statusCode = response.statusCode;
-  var responseHeaders = response.headers;
-  var responseBody = response.body;
-
-
 }
--- lib/map/mapInput.dart
+++ lib/map/mapInput.dart
@@ -1,38 +1,135 @@
+// ignore_for_file: prefer_const_constructors
+import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
+import 'package:geolocator/geolocator.dart';
+import 'package:location/location.dart';
+import 'package:http/http/dart' as http;
 
-class MapInput extends StatelessWidget {
-  const MapInput({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context){
-    return Row(
-      children: <Widget>[
-        TogoInput(),
-
-      ],
-    )
-  }
-}
-
-class TogoInput extends StatefulWidget {
-  const TogoInput({Key? key}) : super(key: key);
+class MapApp extends StatelessWidget{
+  const MapApp({Key? key}) : super(key: key);
 
   @override
-  State<TogoInput> createState() => TogoInputState();
-}
-
-class TogoInputState extends State<TogoInput>{
-
-  const
-
-  @override
-  Widget build(BuildContext context){
-    return TextField(
-      controller: textController,
-      decoration: InputDecoration(
-        border: OutlineInputBorder(),
-        labelText: 'Where to go?',
+  Widget build(BuildContext context) {
+    return Center(
+      child: MaterialApp(
+        title: 'Google Maps',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MapSample(),
       ),
-    )
+    );
+    // TODO: implement build
+    throw UnimplementedError();
   }
+
+
+}
+
+class MapSample extends StatefulWidget {
+  const MapSample({Key? key}) : super(key: key);
+
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+
+
+
+
+
+class MapSampleState extends State<MapSample> {
+  List<Marker> _markers = [];
+
+  @override
+  void initState(){
+    _markers.add(Marker(
+        markerId: MarkerId("first marker"),
+        draggable: true,
+        onTap: () => print("Tappend"),
+        position: LatLng(
+            37.5283169,
+            126.9294254
+        )
+    ));
+  }
+
+
+  @override
+  Widget build(BuildContext context){
+    // ignore: sized_box_for_whitespace
+    // med => media query calculate
+    final med = MediaQuery.of(context).size;
+    final textController = TextEditingController();
+    String? textToFind;
+    // _locationData = locateUser();
+
+    return Scaffold(
+
+        body: Column(
+          children: <Widget>[
+            Container(
+              width: med.width,
+              // height: MediaQuery.of(context).size.height,
+              // width : 300,
+              height: med.height*0.6,
+              child: GoogleMap(
+                mapType: MapType.normal,
+                markers: Set.from(_markers),
+                initialCameraPosition: CameraPosition(
+                  target: LatLng(
+                    // _locationData.latitude,
+                    // _locationData.longitude
+                      37.5283169,
+                      126.9294254
+                  ),
+                  zoom: 18,
+                ),
+              ),
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    controller: textController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Where to go?',
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 20.0,
+                ),
+                OutlinedButton(
+                  onPressed: (){
+                    textToFind = textController.text;
+                    //Respond to button press
+                  },
+                  child: Text("Find"),
+                )
+              ],
+            )
+
+
+          ],
+        )
+    );
+
+  }
+
+}
+
+//location data가져오는 부분
+void getLocationData() async {
+  String url = "";
+  var response = await http.get(url);
+  var statusCode = response.statusCode;
+  var responseHeaders = response.headers;
+  var responseBody = response.body;
+
+
 }
