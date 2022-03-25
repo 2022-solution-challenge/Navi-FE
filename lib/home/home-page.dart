fb98a16e56afb3dc3d5a6c9542cb027b1bc82931
pell13 <peixueying@gmail.com>
2022-03-26 00:03:06 +0900

    Fix connet  widget design

--- lib/home/home-page.dart
+++ lib/home/home-page.dart
@@ -3,6 +3,7 @@ import 'demo-card.dart';
 import 'items.dart';
 import 'record-holder.dart';
 import 'record.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -18,14 +19,64 @@ class HomePage extends StatelessWidget {
 }
 
 Widget _signUpParents() {
-  return Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
-      color: Colors.purple.withOpacity(1),
-      child: Container(
-          constraints: const BoxConstraints.expand(height: 80, width: 300),
-          child: const Center(
-            child: Text('부모 등록'),
-          )));
+  return Container(
+     height: 100,
+     width: 344,
+     margin: EdgeInsets.only(right: 10, left:10, top:10),
+     decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(28),
+                        color: Color.fromARGB(255, 195, 17, 17),
+                      ),
+     child: Stack(
+       children: <Widget>[
+            // Positioned(
+            //   child: SvgPicture.asset(cards[index].cardElementTop),
+            // ),
+            // Positioned(
+            //   bottom: 0,
+            //   right: 0,
+            //   child:
+            //       SvgPicture.asset(cards[index].cardElementBottom),
+            // ), 사진을 넣는 부분
+            Positioned(
+              left: 29,
+              top: 60,
+              child: Text(
+                "Connect with parents",
+                style: GoogleFonts.inter(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white),
+              ),
+            ),
+            const Positioned(
+              right: 21,
+              top: 20,
+              child: Icon(Icons.arrow_right, color: Colors.white70, size: 27),
+            ),
+            Positioned(
+              left: 29,
+              top: 20,
+              child: Text(
+                '카드를 눌러 부모님과 연동할 수 있어요',
+                style: GoogleFonts.inter(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white70),
+              ),
+            ),
+          ]
+          )
+        );
+  
+  // Card(
+  //     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
+  //     color: Colors.purple.withOpacity(1),
+  //     child: Container(
+  //         constraints: const BoxConstraints.expand(height: 80, width: 300),
+  //         child: const Center(
+  //           child: Text('부모 등록'),
+  //         )));
 }
 
 Widget _myRecords() {
@@ -47,7 +98,8 @@ Widget _myRecords() {
 Widget _whereToGo() {
   List<DemoCard> _cards = items.map((Item _item) => DemoCard(_item)).toList();
 
-  return  Container(
+  return  
+  Container(
       child: Column(children: <Widget>[
     Text('길 찾아가기'),
     Stack(alignment: AlignmentDirectional.topStart, children: <Widget>[
