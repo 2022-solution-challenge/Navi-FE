08d72aceb123f223fc62f72e65f0e7b8020eb7c3
hyojaeKwon <khj010909@gm.gist.ac.kr>
2022-03-27 05:08:13 +0900

    login and signin implemented

--- lib/intro/login.dart
+++ lib/intro/login.dart
@@ -1,13 +1,74 @@
-import 'dart:convert';
-import 'dart:io';
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' show jsonDecode;
-
+import 'package:flutter/foundation.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter/material.dart';
 import 'register.dart';
 import 'package:flutter_test_app/main.dart';
 
-class LoginScreen extends StatelessWidget {
+
+class LoginScreen extends StatefulWidget{
+
+  @override
+  State<LoginScreen> createState() => LoginScreenState();
+}
+
+class LoginScreenState extends State<LoginScreen> {
+
+  late FToast fToast;
+
+  late String userName;
+  late String passWord;
+
+  final userNameController = TextEditingController();
+  final passWordController = TextEditingController();
+
+  @override
+  void initState(){
+    super.initState();
+    fToast = FToast();
+    fToast.init(context);
+  }
+
+  void showToast(String msg){
+    Fluttertoast.showToast(
+      msg: msg,
+      toastLength: Toast.LENGTH_LONG,
+      fontSize: 14,
+      backgroundColor: Colors.red.shade400,
+    );
+  }
+
+  //로그인 코드
+  _login() async {
+    //로그인 파라미터 username/password
+    var param = <String, String>{
+      "password": passWordController.text,
+      "username": userNameController.text
+    };
+    var url = Uri.https(
+        "solution-challenge-hb6fjqbi3q-du.a.run.app",
+        "/users/login",
+        param
+    );
+
+
+    final response = await http.get(url);
+    debugPrint(response.statusCode.toString());
+    if(response.statusCode == 200){
+      debugPrint(response.body.toString());
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => MyStatefulWidget()));
+      return;
+    }
+    showToast("please check your name and password");
+    return;
+  }
+
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -33,16 +94,20 @@ class LoginScreen extends StatelessWidget {
             Container(
               alignment: Alignment.center,
               margin: EdgeInsets.symmetric(horizontal: 40),
-              child: const TextField(
-                decoration: InputDecoration(labelText: "Username"),
+              child: TextField(
+                controller: userNameController,
+                decoration: const InputDecoration(
+                    labelText: "Username"
+                ),
               ),
             ),
             SizedBox(height: size.height * 0.03),
             Container(
               alignment: Alignment.center,
               margin: EdgeInsets.symmetric(horizontal: 40),
-              child: const TextField(
-                decoration: InputDecoration(labelText: "Password"),
+              child: TextField(
+                controller: passWordController,
+                decoration: const InputDecoration(labelText: "Password"),
                 obscureText: true,
               ),
             ),
@@ -60,10 +125,17 @@ class LoginScreen extends StatelessWidget {
               margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
               child: ElevatedButton(
                 onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => MyStatefulWidget()));
+
+                  if(userNameController.text == "" || userNameController.text == null) {
+                    showToast("please enter your name");
+                    return;
+                  }
+                  if(passWordController == "" || passWordController.text == null){
+                    showToast("please enter your password");
+                    return;
+                  }
+                  _login();
+
                 },
                 style: ButtonStyle(
                     shape: MaterialStateProperty.all<RoundedRectangleBorder>(
@@ -95,8 +167,8 @@ class LoginScreen extends StatelessWidget {
               margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
               child: GestureDetector(
                 onTap: () => {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => RegisterScreen()))
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => RegisterScreen())),
                 },
                 child: Text(
                   "Don't Have an Account? Sign up",
--- lib/intro/register.dart
+++ lib/intro/register.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'login.dart';
+import 'package:http/http.dart' as  http;
 
 class RegisterScreen extends StatefulWidget{
 
@@ -8,145 +12,203 @@ class RegisterScreen extends StatefulWidget{
 }
 class RegisterScreenState extends State<RegisterScreen> {
 
-  late String name;
-  late String number;
-  late String email;
-  late String password;
+  late FToast fToast;
 
 
+  final nameController = TextEditingController();
+  final numberController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void initState(){
+    super.initState();
+    fToast = FToast();
+    fToast.init(context);
+  }
+
+  void showToast(String msg){
+    Fluttertoast.showToast(
+      msg: msg,
+      toastLength: Toast.LENGTH_LONG,
+      fontSize: 14,
+      backgroundColor: Colors.red.shade400,
+    );
+  }
+
+  //회원가입 코드
+  _signUp() async {
+    String url = "https://solution-challenge-hb6fjqbi3q-du.a.run.app/users";
+
+    http.Response response = await http.post(
+      Uri.parse(url),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(<String, String>{
+        "email" : emailController.text,
+        "password" : passwordController.text,
+        "phoneNumber" : numberController.text,
+        "username" : nameController.text
+      }),
+    );
+
+    debugPrint(nameController.text);
+    debugPrint(passwordController.text);
+    Navigator.pop(context);
+  }
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              alignment: Alignment.centerLeft,
-              padding: EdgeInsets.symmetric(horizontal: 40),
-              child: Text(
-                "REGISTER",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFF2661FA),
-                  fontSize: 36
-                ),
-                textAlign: TextAlign.left,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            alignment: Alignment.centerLeft,
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: const Text(
+              "REGISTER",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF2661FA),
+                fontSize: 36
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+
+          SizedBox(height: size.height * 0.01),
+
+          Container(
+            alignment: Alignment.center,
+            margin: const EdgeInsets.symmetric(horizontal: 40),
+            child: TextField(
+              controller: nameController,
+              decoration: const InputDecoration(
+                labelText: "Name"
               ),
             ),
+          ),
 
-            SizedBox(height: size.height * 0.01),
+          SizedBox(height: size.height * 0.01),
 
-            Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.symmetric(horizontal: 40),
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: "Name"
-                ),
+          Container(
+            alignment: Alignment.center,
+            margin: const EdgeInsets.symmetric(horizontal: 40),
+            child: TextField(
+              controller: numberController,
+              decoration: const InputDecoration(
+                labelText: "Mobile Number"
               ),
             ),
+          ),
 
-            SizedBox(height: size.height * 0.01),
+          SizedBox(height: size.height * 0.01),
 
-            Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.symmetric(horizontal: 40),
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: "Mobile Number"
-                ),
+          Container(
+            alignment: Alignment.center,
+            margin: const EdgeInsets.symmetric(horizontal: 40),
+            child: TextField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                labelText: "Email"
               ),
             ),
+          ),
 
-            SizedBox(height: size.height * 0.01),
+          SizedBox(height: size.height * 0.01),
 
-            Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.symmetric(horizontal: 40),
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: "Email"
-                ),
+          Container(
+            alignment: Alignment.center,
+            margin: const EdgeInsets.symmetric(horizontal: 40),
+            child: TextField(
+              controller: passwordController,
+              decoration: const InputDecoration(
+                labelText: "Password"
               ),
+              obscureText: true,
             ),
+          ),
 
-            SizedBox(height: size.height * 0.01),
+          SizedBox(height: size.height * 0.03),
 
-            Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.symmetric(horizontal: 40),
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: "Password"
-                ),
-                obscureText: true,
-              ),
-            ),
+          Container(
+            alignment: Alignment.centerRight,
+            margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+            child: ElevatedButton(
+              onPressed: () {
+                if(nameController.text == ""){
+                  showToast("please enter your name");
+                  return;
+                }
+                if(numberController.text == ""){
+                  showToast("please enter your phone number");
+                  return;
+                }
+                if(emailController.text == ""){
+                  showToast("please enter your email");
+                  return;
+                }
+                if(passwordController.text == ""){
+                  showToast("please enter your password");
+                  return;
+                }
+                _signUp();
 
-            SizedBox(height: size.height * 0.03),
-
-            Container(
-              alignment: Alignment.centerRight,
-              margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-              child: ElevatedButton(
-                onPressed: () {
-
-                },
-                style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(80.0),
-                          //side: BorderSide(color: Colors.red)
-                        )
+              },
+              style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(80.0),
+                        //side: BorderSide(color: Colors.red)
                       )
-                    ),
-                child: Container(
-                  alignment: Alignment.center,
-                  height: 50.0,
-                  width: size.width * 0.5,
-                  decoration: new BoxDecoration(
-                    borderRadius: BorderRadius.circular(80.0),
-                    gradient: new LinearGradient(
-                      colors: [
-                        Color.fromARGB(255, 255, 136, 34),
-                        Color.fromARGB(255, 255, 177, 41)
-                      ]
                     )
                   ),
-                  padding: const EdgeInsets.all(0),
-                  child: Text(
-                    "SIGN UP",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold
-                    ),
+              child: Container(
+                alignment: Alignment.center,
+                height: 50.0,
+                width: size.width * 0.5,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(80.0),
+                  gradient: const LinearGradient(
+                    colors: [
+                      Color.fromARGB(255, 255, 136, 34),
+                      Color.fromARGB(255, 255, 177, 41)
+                    ]
+                  )
+                ),
+                padding: const EdgeInsets.all(0),
+                child: const Text(
+                  "SIGN UP",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold
                   ),
                 ),
               ),
             ),
+          ),
 
-            Container(
-              alignment: Alignment.centerRight,
-              margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-              child: GestureDetector(
-                onTap: () => {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen()))
-                },
-                child: Text(
-                  "Already Have an Account? Sign in",
-                  style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.bold,
-                    color: Color(0xFF2661FA)
-                  ),
+          Container(
+            alignment: Alignment.centerRight,
+            margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+            child: GestureDetector(
+              onTap: () => {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen()))
+              },
+              child: const Text(
+                "Already Have an Account? Sign in",
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF2661FA)
                 ),
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
