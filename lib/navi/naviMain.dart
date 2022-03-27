ff912652cb5e30b2354a3a7b3b9d42405d304b60
pell13 <peixueying@gmail.com>
2022-03-28 03:05:16 +0900

    add searchbar widget

--- lib/navi/naviMain.dart
+++ lib/navi/naviMain.dart
@@ -1,24 +1,26 @@
 import 'dart:developer';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'mapModel/mapDest.dart';
 import 'searchLocationModel/searchData.dart';
 import 'searchLocationModel/searchDataModel.dart';
 import 'searchLocationModel/searchDataRepository.dart';
+import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 
-class NaviApp extends StatefulWidget{
+class NaviApp extends StatefulWidget {
   const NaviApp({Key? key}) : super(key: key);
 
   @override
   State<NaviApp> createState() => NaviAppState();
 }
-class NaviAppState extends State<NaviApp>{
+
+class NaviAppState extends State<NaviApp> {
   bool isLoaded = false;
   SearchData? naviSearchData;
   void setSearchData(String query) async {
-
     //잘못 눌렸을 경우 대비용 null BTN
-    if (query == null){
+    if (query == null) {
       return;
     }
     SearchData? nowData = await SearchRepository().getInfomation(name: query);
@@ -29,11 +31,16 @@ class NaviAppState extends State<NaviApp>{
   }
 
   @override
-  Widget build(BuildContext context){
-    return Center(
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Text("Where to Go? SEARCH"),
+            _description(),
+            Expanded(
+      child: SearchBar(),
+    ),
             SizedBox(
               height: 20.0,
             ),
@@ -43,14 +50,12 @@ class NaviAppState extends State<NaviApp>{
             ),
             SearchResult(searchData: naviSearchData, isLoaded: isLoaded),
           ],
-        )
-    );
+        ));
   }
 }
 
-
 //Make StateFul widget
-class ToGoInput extends StatefulWidget{
+class ToGoInput extends StatefulWidget {
   const ToGoInput(this.changeQuery);
 
   final void Function(String) changeQuery;
@@ -59,37 +64,31 @@ class ToGoInput extends StatefulWidget{
   State<ToGoInput> createState() => ToGoInputState();
 }
 
-class ToGoInputState extends State<ToGoInput>{
-
+class ToGoInputState extends State<ToGoInput> {
   final textController = TextEditingController();
   String textToFind = "";
   @override
-  void initState(){
-
-  }
+  void initState() {}
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Container(
       // height: 300,
       child: Row(
         children: <Widget>[
-          
           Expanded(
             child: TextField(
               controller: textController,
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
-                labelText: 'Where to go?',
+                labelText: 'Search destination',
               ),
             ),
           ),
-
           SizedBox(
             width: 20.0,
           ),
-
           OutlinedButton(
-            onPressed: (){
+            onPressed: () {
               textToFind = textController.text;
               widget.changeQuery(textToFind);
             },
@@ -101,3 +100,135 @@ class ToGoInputState extends State<ToGoInput>{
   }
 }
 
+Widget _description() {
+  return Padding(
+    padding: EdgeInsets.only(left: 16, bottom: 16, top: 60),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Where to Go?',
+          style: GoogleFonts.inter(
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        Text(
+          'Search',
+          style: GoogleFonts.inter(
+            fontSize: 30,
+            fontWeight: FontWeight.w700,
+          ),
+        )
+      ],
+    ),
+  );
+}
+
+class SearchBar extends StatefulWidget {
+  @override
+  _SearchBar createState() => _SearchBar();
+}
+
+class _SearchBar extends State<SearchBar> {
+  static const historyLength = 5;
+
+
+  late List<String> filteredSearchHistory;
+
+  String selectedTerm = "";
+
+
+  FloatingSearchBarController controller =  FloatingSearchBarController();
+
+  @override
+  void initState() {
+    super.initState();
+    controller = FloatingSearchBarController();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingSearchBar(
+      controller: controller,
+      transition: CircularFloatingSearchBarTransition(),
+      physics: BouncingScrollPhysics(),
+      title: Text(
+        selectedTerm ?? 'The Search App',
+        style: Theme.of(context).textTheme.headline6,
+      ),
+      hint: 'Search and find out...',
+      actions: [
+        FloatingSearchBarAction.searchToClear(),
+      ],
+      onSubmitted: (query) {
+        setState(() {
+          selectedTerm = query;
+        });
+        controller.close();
+      },
+      builder: (context, transition) {
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Material(
+            color: Colors.white,
+            elevation: 4,
+            child: Builder(
+              builder: (context) {
+                if (controller.query.isEmpty) {
+                  return Container(
+                    height: 56,
+                    width: 100,
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Start searching',
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                  );
+                } else {
+                  return Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: filteredSearchHistory
+                        .map(
+                          (term) => ListTile(
+                            title: Text(
+                              term,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            leading: const Icon(Icons.history),
+                            trailing: IconButton(
+                              icon: const Icon(Icons.clear),
+                              onPressed: () {
+                                setState(() {
+
+                                });
+                              },
+                            ),
+                            onTap: () {
+                              setState(() {
+                                selectedTerm = term;
+                              });
+                              controller.close();
+                            },
+                          ),
+                        )
+                        .toList(),
+                  );
+                }
+              },
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
