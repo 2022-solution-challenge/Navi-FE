dbb79869b000db94eb7911e5654de7ad7c60a3db
Seolyeong Bae <80435616+pell13@users.noreply.github.com>
2022-04-01 12:02:07 +0900

    Fix/final version (#36)
    * Fix texts
    
    * Revise Btn Icon
    
    * Fix Login Button
    
    * Change initial Position on bookmark
    
    * Fix App name, App logo
    
    * Change mapDest appbar Text

--- lib/navi/mapModel/mapDest.dart
+++ lib/navi/mapModel/mapDest.dart
@@ -14,7 +14,7 @@ class NaviMainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Navigation Demo',
+      title: 'Navi',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -173,7 +173,6 @@ class _MapScreenState extends State<MapScreen> {
 
   bool isMarkerNearPath(Marker marker) {
     //LatLng이 정의된 게 달라서 몹시 번거롭다..
-
     final pointMp =
         mp.LatLng(marker.position.latitude, marker.position.longitude);
     return mp.PolygonUtil.isLocationOnPath(pointMp, pathPointList, false);
